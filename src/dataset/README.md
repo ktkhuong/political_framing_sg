86df7e253344492d55047906f45328d99c32e5e6
kien-trung <kientrung.khuong@officience.com>
2022-02-07 09:38:40 +0000

    update parliament

--- src/dataset/README.md
+++ src/dataset/README.md
@@ -1,2 +1,6 @@
 Selenium
 ChromeDriver
+
+## Enable Long Paths in Windows 10, Version 1607, and Later
+`Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem\LongPathsEnabled (Type: REG_DWORD)` must exist and be set to 1
+
---
+++ src/dataset/run_parliament.bat
@@ -0,0 +1,18 @@
+@echo off
+rem python parliament.py -p 1
+start cmd
+
+start cmd /k "python parliament.py -p 1"
+start cmd /k "python parliament.py -p 2"
+start cmd /k "python parliament.py -p 3"
+start cmd /k "python parliament.py -p 4"
+start cmd /k "python parliament.py -p 5"
+start cmd /k "python parliament.py -p 6"
+start cmd /k "python parliament.py -p 7"
+start cmd /k "python parliament.py -p 8"
+start cmd /k "python parliament.py -p 9"
+start cmd /k "python parliament.py -p 10"
+start cmd /k "python parliament.py -p 11"
+start cmd /k "python parliament.py -p 12"
+start cmd /k "python parliament.py -p 13"
+start cmd /k "python parliament.py -p 14"
