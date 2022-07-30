019cab5ff73dfc2166c4dac0c89141f60e11bcc0
ktkhuong <ktkhuong@live.com>
2022-07-30 12:53:22 +0100

    attempt to assign extra docs to leaf topcis

--- models/TimeWindow.py
+++ models/TimeWindow.py
@@ -1,20 +1,24 @@
 import numpy as np
 import pickle, logging
 import pandas as pd
+from scipy import spatial
 from nmf import choose_topics
 from sklearn.preprocessing import normalize
 
 class TimeWindow:
     OUT_PATH = "out"
 
-    def __init__(self, id, speech_ids, tfidf_matrix, vocab):
+    def __init__(self, id, speech_ids, tfidf_matrix, vocab, alpha = 0.05, m = 25):
         self.id = id
         self.speech_ids = speech_ids
         self.tfidf_matrix = tfidf_matrix # TF-IDF of the time window
         self.vocab = vocab
+        self.alpha = alpha
+        self.m = m
         self.topics = [] # list of Topic
         self.coherence = 0
         self.children = []
+        self.extras = []
 
     def fit(self, coherence_model, min_k, max_k):
         logger = logging.getLogger(__name__)
@@ -32,16 +36,24 @@ class TimeWindow:
         self.topics = topics
         self.coherence = coherence
 
-        if self.id.count("/") == 0:
-            self.fit_children(coherence_model, 5, 15)
- 
-        """ if self.id.count("/") == 0: # only applicable to root
-            logger.info(f"Assign extra topics to leaf topic...")
-            topics = self.topics[:]
-            for child in self.children: 
-                topics += child.topics[:] """
-
         if self.id.count("/") == 0: # only applicable to root
+            self.fit_children(coherence_model, 5, 15)
+
+            logger.info(f"Assign extra topics to leaf topic...")
+            leaves = self.all_leaves
+            for row in self.extra_speeches:
+                speech_terms = self.tfidf_matrix[row]
+                best_topic = None
+                best_similarity = 0
+                for topic in leaves:
+                    topic_terms = topic.term_weights
+                    # cosine similiarity
+                    similarity = 1 - spatial.distance.cosine(speech_terms, topic_terms)
+                    if similarity > best_similarity:
+                        best_topic = topic
+                        best_similarity = similarity
+                self.extras.append((self.speech_ids[row], best_topic.id, best_similarity))
+
             self.save(f"{self.OUT_PATH}/{self.id}.pkl")
         
         logger.info(f"{self.id}: {self.num_speeches} speeches; {self.num_topics} topics; coherence = {self.coherence};")
@@ -66,13 +78,6 @@ class TimeWindow:
             self.children.append(child)
 
     @property
-    def all_topics(self):
-        topics = self.topics[:]
-        for child in self.children:
-            topics += child.all_topics
-        return topics
-
-    @property
     def num_speeches(self):
         return self.tfidf_matrix.shape[0]
 
@@ -90,33 +95,69 @@ class TimeWindow:
 
     @property
     def extra_speeches(self):
+        """
+        speeches that can not be assigned to any topic, i.e. their correlation < alpha
+        """
         max_weights = np.max(self.W, axis=1)
-        nonzero, *_ = np.where(max_weights < 0.05)
+        nonzero, *_ = np.where(max_weights < self.alpha)
         return nonzero
 
     @property
     def assigned_speeches(self):
+        """
+        speeches that can be assigned to a topic, i.e. their correlation >= alpha
+        """
         max_weights = np.max(self.W, axis=1)
-        nonzero, *_ = np.asarray(max_weights >= 0.05).nonzero()
+        nonzero, *_ = np.asarray(max_weights >= self.alpha).nonzero()
         return nonzero
 
     @property
+    def all_topics(self):
+        """
+        topics of this time windows & its children
+        """
+        topics = self.topics[:]
+        for child in self.children:
+            topics += child.all_topics
+        return topics
+
+    @property
+    def all_leaves(self):
+        """
+        leaf topics of this time windows & its children
+        """
+        topics = [self.topics[i] for i in self.leaf_topics]
+        for child in self.children:
+            topics += child.all_leaves
+        return topics
+
+    @property
     def popular_topics(self):
+        """
+        topics that have more than m speeches assigned to
+        return index of those topics
+        """
         rows = self.assigned_speeches
         W = self.W[rows]
         _, n_topics = W.shape
         x = np.argmax(W, axis=1)
         hist, _ = np.histogram(x, bins=range(n_topics+1))
-        return np.array([i for i, freq in enumerate(hist) if freq > 25])
+        return np.array([i for i, freq in enumerate(hist) if freq > self.m])
+        #return np.where(hist > self.m)
     
     @property
     def leaf_topics(self):
+        """
+        topics that have less than or equal to m speeches assigned to
+        return index of those topics
+        """
         rows = self.assigned_speeches
         W = self.W[rows]
         _, n_topics = W.shape
         x = np.argmax(W, axis=1)
         hist, _ = np.histogram(x, bins=range(n_topics+1))
-        return np.array([i for i, freq in enumerate(hist) if freq < 25])
+        return np.array([i for i, freq in enumerate(hist) if freq < self.m])
+        #return np.where(hist <= self.m)
 
     @property
     def speech2topic(self):
@@ -124,14 +165,16 @@ class TimeWindow:
         Assuming a single membership model, i.e. each speech has 1 topic with the highest weight 
         """
         topics = np.argmax(self.W[self.assigned_speeches], axis=1)
-        speech2topic = []
-        for speech, topic in zip(self.assigned_speeches, topics):
-            speech2topic.append((self.speech_ids[speech], self.topics[topic].id, self.W[speech, topic]))
+        speech2topic = [(self.speech_ids[speech], self.topics[topic].id, self.W[speech, topic]) for speech, topic in zip(self.assigned_speeches, topics)]
         for child in self.children:
             speech2topic += child.speech2topic
+        speech2topic += self.extras
         return speech2topic
 
     def top_term_weights(self, n_top):
+        """
+        weight of top terms of all topics
+        """
         return [topic.top_term_weights(n_top) for topic in self.all_topics]
 
     def save(self, path):
