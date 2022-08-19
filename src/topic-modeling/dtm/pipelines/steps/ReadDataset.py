db43fd846130c6c7bfadb7def459fc8e6267954f
ktkhuong <ktkhuong@live.com>
2022-08-19 10:03:49 +0100

    remove debugging code

--- src/topic-modeling/dtm/pipelines/steps/ReadDataset.py
+++ src/topic-modeling/dtm/pipelines/steps/ReadDataset.py
@@ -13,7 +13,6 @@ class ReadDataset(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
-        """
         logger = logging.getLogger(__name__)
 
         frames = [pd.read_csv(f"{self.DATASET_PATH}/{f}", usecols=["date", "quarter", "section", "title", "member", "preprocessed_speech", "party"])
@@ -23,14 +22,4 @@ class ReadDataset(BaseEstimator, TransformerMixin):
         if self.party != 'all':
             df = df[df["party"] == self.party]
         logger.message(f"Dataset shape: {df.shape}")
-        return df
-        """
-        logger = logging.getLogger(__name__)
-
-        df = pd.read_csv("sgparl.csv", usecols=["date", "quarter", "section", "title", "member", "preprocessed_speech", "party"])
-        logger.message(f"Dataset shape all: {df.shape}")
-        df['date'] = pd.to_datetime(df['date'])
-        if self.party != 'all':
-            df = df[df["party"] == self.party]
-        logger.message(f"Dataset shape {self.party}: {df.shape}")
         return df
